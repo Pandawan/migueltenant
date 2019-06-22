5386f415b336578b5e0891b541e9842d8628b10a
PandawanFr <pandawanfr@gmail.com>
2019-06-22 00:05:56 -0700

    Add CNAME copying from src

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,9 @@ module.exports = function (eleventyConfig) {
   // Optimize all of the images automatically
   optimizeImages();
 
+  
+  eleventyConfig.addPassthroughCopy("src/CNAME");
+
   return {
     dir: {
       input: 'src',
