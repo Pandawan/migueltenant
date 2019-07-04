96f040f01775e12f0d849fcd9e78517b5fc5bae8
PandawanFr <pandawanfr@gmail.com>
2019-07-04 12:25:59 -0700

    Main blog page + macros

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
+const util = require('util');
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginBetterSlug = require("@borisschapira/eleventy-plugin-better-slug");
+const excerpt = require('eleventy-plugin-excerpt');
 
 const optimizeImages = require('./tools/optimize-images');
 
@@ -9,9 +11,14 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginBetterSlug);
+  eleventyConfig.addPlugin(excerpt);
   
   eleventyConfig.addPassthroughCopy("src/CNAME");
   eleventyConfig.addPassthroughCopy("src/js/");
+
+  eleventyConfig.addFilter("JsonStringify", function(value) {
+    return util.inspect(value);
+  });
 
   return {
     dir: {
---
+++ src/blog/articles/random-page.md
@@ -0,0 +1,16 @@
+---
+layout: articlePage
+title: Random page
+description: Hello World
+tags: article
+---
+
+Nothing to see here.
+
+![img](https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000)
+
+```js
+// This is a comment
+let test = "hello world";
+test.replace('hello', 'hi');
+```
