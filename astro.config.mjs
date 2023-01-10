1129d434ef8942ece7bc742843fc4db5d8bb10d7
Miguel <migueltenant@gmail.com>
2023-01-09 18:26:44 -0800

    Add Astro Integrations
    - Add Tailwind
    - Add Astro Sitemap
    - Add Astro Image
    - Add Prettier
    - Add GitHub Pages auto-deploy on push
    - Add support for custom domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sitemap(),
+  ],
+  site: "https://migueltenant.com",
+});
