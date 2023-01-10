6d2fa450cd46f4e71bcdcb39096b1cb28839459c
Miguel <migueltenant@gmail.com>
2023-01-10 11:53:47 -0800

    Basic HTML setup

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Global constants for site rendering
+
+export const SITE_TITLE = "Miguel Tenant de La Tour";
+export const SITE_DESCRIPTION =
+  "I'm a 21-year old EECS student, and I love building things.";
