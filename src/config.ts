b7bac97dbc9933bb8b6b2305b9a39175d348db70
Miguel <migueltenant@gmail.com>
2023-01-25 21:35:13 -0800

    Add Footer

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,8 @@
 export const SITE_TITLE = "Miguel Tenant de La Tour";
 export const SITE_DESCRIPTION =
   "I'm a 21-year old EECS student, and I love building things.";
+
+export const SOCIAL_TWITTER = "https://twitter.com/PandawanMT";
+export const SOCIAL_MAIL = "mailto:migueltenant@gmail.com";
+export const SOCIAL_GITHUB = "https://github.com/Pandawan";
+export const SOCIAL_LINKEDIN = "https://linkedin.com/in/migueltenant";
