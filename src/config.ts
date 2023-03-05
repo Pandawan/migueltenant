185895912f3053760e050d0ebd1c4a2951601623
Miguel <migueltenant@gmail.com>
2023-03-04 21:03:53 -0800

    Reusable components & WIP Projects Section
    - Add (WIP) projects section
    - Re-organize components folder
    - Add reusable components to avoid style repetition

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,8 @@ export const SITE_TITLE = "Miguel Tenant de La Tour";
 export const SITE_DESCRIPTION =
   "I'm a 21-year old EECS student, and I love building things.";
 
+export const LINK_RESUME =
+  "https://docs.google.com/document/d/1Toj0yawmd6Q0mNUubzn1Vxf5t4hp5KJHGaaDo-Rwgcg/export?format=pdf";
 export const SOCIAL_TWITTER = "https://twitter.com/PandawanMT";
 export const SOCIAL_MAIL = "mailto:migueltenant@gmail.com";
 export const SOCIAL_GITHUB = "https://github.com/Pandawan";
