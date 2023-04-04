7d835d52b115c8f66b517a4e0912f2778b326690
Miguel <migueltenant@gmail.com>
2023-04-03 19:42:15 -0700

    Add Blog (WIP)
    - Add infrastructure for blogs
    - Add blog post listing page

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,11 @@ export const SITE_TITLE = "Miguel Tenant de La Tour";
 export const SITE_DESCRIPTION =
   "I'm a 21-year old EECS student, and I love building things.";
 
+export const BLOG_TITLE = "Miguel's Blog";
+export const BLOG_DESCRIPTION =
+  "Random thoughts of a 21-year old student and software engineer.";
+export const BLOG_PATH = "/blog";
+
 export const LINK_RESUME =
   "https://docs.google.com/document/d/1Toj0yawmd6Q0mNUubzn1Vxf5t4hp5KJHGaaDo-Rwgcg/export?format=pdf";
 export const SOCIAL_TWITTER = "https://twitter.com/PandawanMT";
