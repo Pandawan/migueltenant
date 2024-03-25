136d83ee6c5e74f7b699ee9cd3e02ed50e73192a
Miguel <migueltenant@gmail.com>
2024-03-24 17:50:13 -0700

    Upgrade to Astro 4, minor formatting changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     sitemap(),
   ],
   site: "https://migueltenant.com",
   outDir: "./docs",
-  // TODO: Switch to astro/image with the Astro 2.1 Experimental Assets
 });
