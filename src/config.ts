412bdde6b2f3332ecec3522995dee5e46827ff1d
Miguel Tenant de La Tour <migueltenant@gmail.com>
2025-05-10 22:10:24 -0700

    More astro upgrade

--- src/config.ts
+++ src/config.ts
@@ -2,16 +2,16 @@
 
 export const SITE_TITLE = "Miguel Tenant de La Tour";
 export const SITE_DESCRIPTION =
-  "I'm a 22-year old developer, and I love building things.";
+  "I'm a 23-year old developer, and I love building things.";
 
-export const BLOG_TITLE = "Miguel's Blabber";
+export const BLOG_TITLE = "Miguel is Writing";
 export const BLOG_DESCRIPTION =
-  "Random thoughts of a foolish 22-year old with a lot of ambition.";
-export const BLOG_PATH = "/blog";
+  "Miguel's random thoughts...";
+export const BLOG_PATH = "/writing";
 
 export const LINK_RESUME =
   "https://docs.google.com/document/d/1Toj0yawmd6Q0mNUubzn1Vxf5t4hp5KJHGaaDo-Rwgcg/export?format=pdf";
-export const SOCIAL_TWITTER = "https://twitter.com/PandawanMT";
+export const SOCIAL_BSKY = "https://bsky.app/profile/miguel.is";
 export const SOCIAL_MAIL = "mailto:migueltenant@gmail.com";
 export const SOCIAL_GITHUB = "https://github.com/Pandawan";
 export const SOCIAL_LINKEDIN = "https://linkedin.com/in/migueltenant";
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,8 @@
 import { z, defineCollection } from "astro:content";
+import { glob } from 'astro/loaders';
 
 const blogCollection = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/data/blog" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
